ba3b22a535b5cc5e24cfd892e1a8082215eaf0fd
sebastiangustafsson <sebastian.gustafsson@knowit.fi>
2024-01-29 14:32:16 +0200

    Bun tasks

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { Database } from "bun:sqlite";
+import {faker} from '@faker-js/faker'
 
 const app = express();
 const port = 8080;
+
+// Create db and add Users table.
+const db = new Database("mydb.sqlite", { create: true });
+db.query(  `CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  );`,).run();
 
 interface SubmitRequestBody {
   message: string;
@@ -28,7 +37,7 @@ app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   // 1. TASK: faker
-  res.send("Hello!");
+  res.send(faker.hacker.phrase());
 });
 
 app.post('/write', async (req: Request, res: Response) => {
@@ -36,6 +45,8 @@ app.post('/write', async (req: Request, res: Response) => {
     const { message } = req.body as SubmitRequestBody;
 
     // 2. TASK: Write the message to file input.txt
+
+    await Bun.write('input.txt', message)
 
     const responseBody: SubmitResponseBody = { success: true, message };
     res.json(responseBody);
@@ -49,7 +60,7 @@ app.get('/read', async (req: Request, res: Response<SubmitResponseBody>) => {
   try {
 
     // 2. TASK: Read value from file input.txt, and save to message constant.
-    const text = "";
+    const text = await Bun.file('input.txt').text();
 
     if (text) {
       const responseBody: SubmitResponseBody = { success: true, message: text };
@@ -68,8 +79,11 @@ app.post('/register', async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
     // 4. TASK: Hash password using Bun.password
+    const hash = await Bun.password.hash(password);
 
     // 3. TASK: Save user to db
+    const insert = db.prepare("INSERT INTO Users (username, password) VALUES (?, ?)");
+    insert.run(username, hash);
 
     res.json({ success: true });
   } catch (error) {
@@ -82,10 +96,12 @@ app.post('/login', async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
     // 3. TASK: Get user from the db
-    const user = { id: 1, username: "", password: "" } as User;
+    const user= db.prepare('SELECT * FROM Users WHERE username = ?').get(username) as User;
+    //const user = { id: 1, username: "", password: "" } as User;
+
 
     // 4. TASK: Verify password
-    if (user && password === user.password) {
+    if (user && await Bun.password.verify(password, user.password)) {
       res.json({ success: true });
     } else {
       return res.status(401).json({ error: 'Authentication failed.' });
